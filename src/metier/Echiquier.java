eee7779b37a90529ee6289b6da9483dc63b37d0c
Koialle <yoite.hikari@gmail.com>
2017-05-05 09:39:07 +0200

    Ajout du calcul de conflits

--- src/metier/Echiquier.java
+++ src/metier/Echiquier.java
@@ -1,8 +1,10 @@
 package metier;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -14,7 +16,7 @@ public class Echiquier {
     private boolean estSolution;
     private List<Dame> dames;
     private Map<Integer,List<Integer>> listeColonne;
-    private HashMap<Integer,List<Integer>> listeLigne;
+    private Map<Integer,List<Integer>> listeLigne;
     private int nbConflits;
     private int tailleEchiquier;
     private int typeInitialisation; // TODO : replacer par enum
@@ -36,7 +38,76 @@ public class Echiquier {
         return null;
     }
     
+    public int calculeConflits() {
+        int nbConflitsTotal = 0;
+        
+        for (Map.Entry<Integer, List<Integer>> entry : listeColonne.entrySet()) {
+            if (entry.getValue().size() > 0) {
+                int colonne = entry.getKey();
+                for (Integer ligne : entry.getValue()) {
+                    // Dame (colonne, ligne)
+                    int conflits = 0;
+                    
+                    // 1. Conflits de ligne
+                    conflits += listeLigne.get(ligne).size() - 1;
+                    
+                    // 2. Conflits de colonne
+                    conflits += listeColonne.get(colonne).size() - 1;
+                    
+                    // 3. Conflits de diagonale
+                    conflits += this.calculeConflitsDiagonale(colonne, ligne);
+                    
+                    nbConflitsTotal += conflits;
+                }
+            }
+        }
 
+        return nbConflitsTotal;
+    }
+    
+    public int calculeConflitsDiagonale(int colonne, int ligne) {
+        int conflits = 0;
+        int x = 0, y = 0;
+
+        // Diagonale haut-gauche
+        for (x = ligne, y = colonne; x > 0 && y > 0 && x <= tailleEchiquier && y <= tailleEchiquier; x--, y--) {
+            if (listeColonne.get(y).size() > 0) {
+                for (int i : listeColonne.get(y)) {
+                    if (y == i) conflits++;
+                }
+            }
+        }
+
+        // Diagonale haut-droite
+        for (x = ligne, y = colonne; x > 0 && y > 0 && x <= tailleEchiquier && y <= tailleEchiquier; x--, y++) {
+            if (listeColonne.get(y).size() > 0) {
+                for (int i : listeColonne.get(y)) {
+                    if (y == i) conflits++;
+                }
+            }
+        }
+        
+        // Diagonale bas-gauche
+        for (x = ligne, y = colonne; x > 0 && y > 0 && x <= tailleEchiquier && y <= tailleEchiquier; x++, y--) {
+            if (listeColonne.get(y).size() > 0) {
+                for (int i : listeColonne.get(y)) {
+                    if (y == i) conflits++;
+                }
+            }
+        }
+        
+        // Diagonale bas-droite
+        for (x = ligne, y = colonne; x > 0 && y > 0 && x <= tailleEchiquier && y <= tailleEchiquier; x++, y++) {
+            if (listeColonne.get(y).size() > 0) {
+                for (int i : listeColonne.get(y)) {
+                    if (y == i) conflits++;
+                }
+            }
+        }
+        
+        return conflits;
+    }
+    
     public boolean isEstSolution() {
         return estSolution;
     }
